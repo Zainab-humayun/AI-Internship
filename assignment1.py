21e9a4e17ca489846448c05c48de88acf01a0891
Zainab-humayun <zainabhumayunj5@gmail.com>
2024-07-08 18:50:45 +0500

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -16,6 +16,11 @@ def union(set1,set2):
 def main():
     set1={'2','3','4','55','43'}
     set2={'3','1','65','72','55'}
+    #to take user input uncomment code below 
+    # list_set1=(input("Enter elements of first set: ").split()) ##use split to comvert to list
+    # set1=set(list_set1) ##convert to set (removes repeated items)
+    # list_set2=(input("Enter elements of second set: ")).split()
+    # set2= set(list_set2)
     intersec= intersection(set1,set2)
     print("Intersection of the 2 sets is: ", intersec)
     uni=union(set1,set2)
